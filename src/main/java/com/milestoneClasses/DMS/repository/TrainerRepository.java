dd1ec0e2df773a4dce16a794ce98dc1d151bdcf0
Divya_sri_Boppudi <s556453@nwmissouri.edu>
2023-04-27 13:18:25 -0500

    DMS to deploy

---
+++ src/main/java/com/milestoneClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.milestoneClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.milestoneClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
